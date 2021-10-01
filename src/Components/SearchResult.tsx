fbc45048ec84b4fea13037ad04a940d84b78bd0c
yuling <yulinglin@protonmail.com>
2021-10-01 19:01:48 +0800

    refactor: Installed packages for server
    1. Changed require to import.
    2. Added type for API data
    3. Error to be fixed: type unknown is not assignable to type GeoData

--- src/Components/SearchResult.tsx
+++ src/Components/SearchResult.tsx
@@ -1,16 +1,47 @@
 import React, { useContext, useEffect } from 'react';
 import { TravelInfoContext } from '../Contexts/TravelContext';
+let url;
+let city;
+let weatherInfo;
 
 const SearchResult = (): JSX.Element => {
   const travelInfo = useContext(TravelInfoContext);
-  useEffect(() => {
-    console.log(travelInfo!);
-  });
+  const locationInput = travelInfo!.values.destination;
+  const startDate = travelInfo!.values.tripStart;
+  const endDate = travelInfo!.values.tripEnd;
+  const duration = travelInfo!.values.travelDuration;
+
+  const getLocation = (locationInput: string, startDate: string, endDate: string) => {
+    fetch('http://localhost:8080/getGeoname', {
+      method: 'POST',
+      credentials: 'same-origin',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ location: `${locationInput}`, startDate: `${startDate}`, endDate: `${endDate}` }),
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log('response json', json);
+        city = json[0].city_name;
+        url = json[1];
+        weatherInfo = json[0].data;
+
+        if (duration <= 0) {
+          alert('Date input seems not correct');
+          return;
+        } else {
+          console.log('Fetch testing');
+          //Client.updateUi(city, url, startDate, endDate, duration, weatherInfo)
+        }
+      });
+  };
 
   return (
     <div>
-      <h1>{travelInfo!.values.destination}</h1>
-      {travelInfo!.values.travelDuration != 0 ? <h3>{travelInfo!.values.travelDuration}</h3> : null}
+      <h1>{locationInput}</h1>
+      {duration !== 0 ? <h3>{duration}</h3> : null}
     </div>
   );
 };
---
+++ src/server.tsx
@@ -0,0 +1,79 @@
+/*Api URL*/
+import path from 'path';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { Request, Response, Application } from 'express';
+import express from 'express';
+import fetch from 'node-fetch';
+//const geonamesBaseUrl = 'http://api.geonames.org/searchJSON?';
+//const weatherBitUrl = 'https://api.weatherbit.io/v2.0/forecast/daily?';
+//const pixabayUrl = 'https://pixabay.com/api/?key=4772361-58a041a9c4a31b16cbe90fbc1&q=yellow+flowers&image_type=photo';
+const app: Application = express();
+app.use(express.static('dist'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+//const cors = require('cors');
+app.use(cors());
+app.listen(8080);
+
+app.get('/', (req: Request, res: Response) => {
+  //res.sendFile('dist/index.tsx');
+  res.sendFile(path.resolve('src/index.tsx'))
+});
+
+type GeoData = {
+  geonames: GeonamesEntity[];
+}
+
+type GeonamesEntity = {
+  lng: string;
+  lat: string;
+}
+
+type PixabayData = {
+  hits: hitsEntity[];
+}
+
+type hitsEntity = {
+  largeImageURL: string;
+}
+
+app.post('/getGeoname', async (req: Request, res: Response) => {
+  const API_URL = `http://api.geonames.org/searchJSON?q=${req.body.location}&maxRows=1&username=iku124`;
+  const myPromise = await fetch(API_URL);
+
+  try {
+    const myData: GeoData = await myPromise.json();
+    const lng = myData.geonames[0].lng;
+    const lat = myData.geonames[0].lat;
+    const weatherbit = await fetch(
+      `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lng}&days=7&key=25236c54b21347c5acba0d34020a5c84`,
+    );
+    try {
+      const weatherbitPromise = await weatherbit.json();
+      console.log(weatherbitPromise);
+      const pixabay = await fetch(
+        `https://pixabay.com/api/?key=4772361-58a041a9c4a31b16cbe90fbc1&q=${req.body.location}&image_type=photo&editors_choice=true&category=travel`,
+      );
+
+      try {
+        const pixabayPrimose: PixabayData = await pixabay.json();
+        console.log(pixabayPrimose.hits[0]);
+        res.send([weatherbitPromise, pixabayPrimose.hits[0].largeImageURL]);
+      } catch (error) {
+        res.send([weatherbitPromise, '/img/backup.png']);
+        console.log('Something wrong when fetching the photo', error);
+      }
+    } catch (err) {
+      console.log('Error in the second fetch', err);
+    }
+  } catch (error) {
+    alert('Make sure you enter a country or city name');
+    return;
+  }
+});
+
+module.exports = app;
+
