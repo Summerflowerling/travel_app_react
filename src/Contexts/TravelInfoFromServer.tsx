1683bc2896a6d20c7ba9460b9e3f25f6a4d333ed
yuling <yulinglin@protonmail.com>
2021-10-22 09:51:57 +0800

    refactor: Changed the object type to Record<string, unknown> as typescript recommended in TravelInfoFromServer.tsx

--- src/Contexts/TravelInfoFromServer.tsx
+++ src/Contexts/TravelInfoFromServer.tsx
@@ -1,33 +1,33 @@
 import React, { useState, createContext } from 'react';
 
 type TravelInfoFromServerContextProviderProps = {
-    children: React.ReactNode;
-  };
-  
-  export type TravelInfoFromServer = {
-      data: object[];
-      city_name: string;
-      timezone:string
-      
-  };
+  children: React.ReactNode;
+};
 
-export type WeatherDataEntity = {
+export type TravelInfoFromServer = {
+  data: Record<string, unknown>[];
+  city_name: string;
+  timezone: string;
+};
 
-}
-  
-  type ValuesFromServer = {
-    valuesFromServer: TravelInfoFromServer;
-    setValuesFromServer : React.Dispatch<React.SetStateAction<TravelInfoFromServer>>;
-  };
-  
-  export const TravelInfoFromServerContext = createContext<ValuesFromServer | null>(null);
-  
-  export const TravelInfoContextProvider = ({ children }: TravelInfoFromServerContextProviderProps): React.ReactElement => {
-    const [valuesFromServer, setValuesFromServer] = useState<TravelInfoFromServer>({
-        data: [],
-        city_name: "No data yet",
-        timezone:"no data yet"
-    });
-    return <TravelInfoFromServerContext.Provider value={{ valuesFromServer, setValuesFromServer }}>{children}</TravelInfoFromServerContext.Provider>;
-  };
-  
+type ValuesFromServer = {
+  valuesFromServer: TravelInfoFromServer;
+  setValuesFromServer: React.Dispatch<React.SetStateAction<TravelInfoFromServer>>;
+};
+
+export const TravelInfoFromServerContext = createContext<ValuesFromServer | null>(null);
+
+export const TravelInfoFromServerProvider = ({
+  children,
+}: TravelInfoFromServerContextProviderProps): React.ReactElement => {
+  const [valuesFromServer, setValuesFromServer] = useState<TravelInfoFromServer>({
+    data: [],
+    city_name: 'No data yet',
+    timezone: 'no data yet',
+  });
+  return (
+    <TravelInfoFromServerContext.Provider value={{ valuesFromServer, setValuesFromServer }}>
+      {children}
+    </TravelInfoFromServerContext.Provider>
+  );
+};
